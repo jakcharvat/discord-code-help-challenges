a95c831f03f37dfd50acc0142564bfc8104ac066
Jakub Charvat <jakcharvat@gmail.com>
2021-06-06 11:55:20 +0300

    Add WASM Xcode project

---
+++ src/challenge4/src/lib/WASM/WASM/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  WASM
+//
+//  Created by Jakub Charvat on 06.06.2021.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
